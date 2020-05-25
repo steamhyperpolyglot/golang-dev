e8d1214e3921e2856759abc75d1952965a617c58
Sherman Chen <uxdailygeek@gmail.com>
2020-05-25 15:19:57 +0800

    Completed code examples for Chapter 5

--- Go_Workshop/CH05_Functions/Ex505_SummingNumbers/main.go
+++ Go_Workshop/CH05_Functions/Ex505_SummingNumbers/main.go
@@ -1,1 +1,21 @@
-package Ex505_SummingNumbers
+package main
+
+import (
+	"fmt"
+)
+
+func sum(nums ...int) int {
+	total := 0
+	
+	for _, num := range nums {
+		total += num
+	}
+	
+	return total
+}
+
+func main() {
+	i := [] int { 5, 10, 15 }
+	fmt.Println(sum(5, 4))
+	fmt.Println(sum(i...))
+}
--- Go_Workshop/CH05_Functions/fizzBiz/main.go
+++ Go_Workshop/CH05_Functions/fizzBiz/main.go
@@ -1,1 +1,25 @@
-package fizzBiz
+package main
+
+import (
+	"fmt"
+)
+
+func fizzBuzz () {
+	for i := 1; i <= 30; i++ {
+		if i % 15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Main is in control")
+	fizzBuzz()
+	fmt.Println("Back to main")
+}
