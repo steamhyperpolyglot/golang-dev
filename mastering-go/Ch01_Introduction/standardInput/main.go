cb4206ac144fe037ab12f720dfb22c225ce46dda
Sherman Chen <uxdailygeek@gmail.com>
2020-11-13 19:26:47 +0800

    Finished code examples from Chapter 1

--- mastering-go/Ch01_Introduction/standardInput/main.go
+++ mastering-go/Ch01_Introduction/standardInput/main.go
@@ -1,1 +1,18 @@
-package standardInput
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var f *os.File
+	f = os.Stdin
+	defer f.Close()
+	
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(">", scanner.Text())
+	}
+}
