6a17679ee505c79754bbb8e5d639751340ecea7d
Sherman Chen <uxdailygeek@gmail.com>
2021-01-29 02:02:46 +0800

    Added the first few examples in Mastering Go

--- mastering-go/Ch03_DataTypes/executionTime/main.go
+++ mastering-go/Ch03_DataTypes/executionTime/main.go
@@ -1,1 +1,13 @@
-package executionTime
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	time.Sleep(time.Second)
+	duration := time.Since(start)
+	fmt.Println("It took time.Sleep(1)", duration, "to finish.")
+}
