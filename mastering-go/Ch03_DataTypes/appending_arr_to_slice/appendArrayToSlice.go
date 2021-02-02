7284bea3261f5bce6ee65de07678fb891db93220
Sherman Chen <uxdailygeek@gmail.com>
2021-02-02 11:19:18 +0800

    Finished code examples for Chapter 3

--- mastering-go/Ch03_DataTypes/appending_arr_to_slice/appendArrayToSlice.go
+++ mastering-go/Ch03_DataTypes/appending_arr_to_slice/appendArrayToSlice.go
@@ -1,1 +1,19 @@
-package appending_arr_to_slice
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := [] int { 1, 2, 3 }
+	a := [3] int { 4, 5, 6 }
+	
+	ref := a[:]
+	fmt.Println("Existing array:\t", ref)
+	t := append(s, ref...)
+	fmt.Println("New slice:\t\t", t)
+	s = append(s, ref...)
+	fmt.Println("Existing slice:\t", s)
+	s = append(s, s...)
+	fmt.Println("s+s:\t\t\t", s)
+}
--- mastering-go/Ch03_DataTypes/using_time/usingTime.go
+++ mastering-go/Ch03_DataTypes/using_time/usingTime.go
@@ -1,1 +1,23 @@
-package using_time
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Epoch time:", time.Now().Unix())
+	t := time.Now()
+	fmt.Println(t, t.Format(time.RFC3339))
+	fmt.Println(t.Weekday(), t.Day(), t.Month(), t.Year())
+	
+	time.Sleep(time.Second)
+	t1 := time.Now()
+	fmt.Println("Time difference:", t1.Sub(t))
+	
+	formatT := t.Format("01 January 2006")
+	fmt.Println(formatT)
+	loc, _ := time.LoadLocation("Asia/Singapore")
+	sgTime := t.In(loc)
+	fmt.Println("Singapore:", sgTime)
+}
